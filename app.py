8bc27228fe035f1fed798d7916c95f664f88f502
Gavin <gkmusicproject@gmail.com>
2025-06-14 00:21:11 -0400

    Initial commit: Added basic greeting application

---
+++ app.py
@@ -0,0 +1,5 @@
+def greet_user(Gavin):
+    return f"Hello, Gavin! Welcome to MusicApp."
+if __name__ == "__main__":
+    user_name = input("Please enter your name:")
+    print(greet_user(user_name))
